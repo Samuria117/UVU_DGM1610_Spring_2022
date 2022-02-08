d0f71a74ed0805d178e42cf9982aa905c7f4528c
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-02-07 19:54:18 -0700

    Added Script 3

---
+++ My project (1)/Assets/Script3.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+  public string myName
+  // Use this for initialization
+  void Start ()
+    {
+        Debug.Log("I am alive and my name is" + Sam)
+    }
+}
