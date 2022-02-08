1423c921a2a1365c636738a9987a737c5f4338f0
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-02-08 09:19:02 -0700

    Added Start and Awake

---
+++ My project (1)/Assets/AwakeandStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakAndStart : MonoBehaviour
+{
+    void Awake ()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    void Start()
+    {
+        Debug.Log("Start called")
+    }
+}
