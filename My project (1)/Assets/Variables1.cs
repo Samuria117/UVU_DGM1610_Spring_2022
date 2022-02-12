25ce2becf95adae5c4273441fbaaf9f9596e45aa
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-02-11 20:14:58 -0700

    Variables

---
+++ My project (1)/Assets/Variables1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables1 : MonoBehaviour
+{
+    class Pogram
+    {
+        string name = "Sam Zepeda"
+            Console.WriteLine(Sam Zepeda)
+    }
+}
