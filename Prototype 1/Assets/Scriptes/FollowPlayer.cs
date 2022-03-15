cc89410a7589ec6a43d85c1b4c0efd500eb4b52e
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-03-14 21:37:20 -0600

    Added New scripts

--- Prototype 1/Assets/Scriptes/FollowPlayer.cs
+++ Prototype 1/Assets/Scriptes/FollowPlayer.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
+    public FollowPlayer player;
     private Vector3 offset = new Vector3(0, 5, -7);
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         transform.position = palyer.position + offset;
     }
