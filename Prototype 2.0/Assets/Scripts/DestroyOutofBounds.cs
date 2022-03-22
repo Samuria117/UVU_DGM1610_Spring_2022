2f249f78d69b0ed9b00abcfe6467d34f6ce3ad7c
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-03-21 22:59:34 -0600

    Added new and fixed scripts

--- Prototype 2.0/Assets/Scripts/DestroyOutofBounds.cs
+++ Prototype 2.0/Assets/Scripts/DestroyOutofBounds.cs
@@ -20,8 +20,7 @@ public class DestroyOutofBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
            Destroy(gameObject);
-        } 
-        else if (transform.position.z < lowerBound)
+        } else if (transform.position.z < lowerBound)
         { 
            Debug.Log("Game Over!");  
            Destroy(gameObject);
