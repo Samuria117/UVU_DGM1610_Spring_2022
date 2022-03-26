3446ecb0358c2dd7c3cc67c617509b28f899e773
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-03-25 20:31:40 -0600

    Added and fixed jumping scripts

--- Prototype 3.0/Assets/Scripts/SpawnManager.cs
+++ Prototype 3.0/Assets/Scripts/SpawnManager.cs
@@ -8,8 +8,7 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
-    private PlayerController playerControllerScript;
-    
+    public PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +24,9 @@ public class SpawnManager : MonoBehaviour
     
     void SpawnObstacle ()
     {
-        if (PlayerControllerScript.gameOver == false)
-        {
+         if (playerControllerScript.gameOver == false)
+         {
             Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-        }
+         }
     }
 }
