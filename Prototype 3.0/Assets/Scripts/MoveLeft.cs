4828a788b3f6cc85515664a856ae05d2185c50e0
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-03-26 22:53:50 -0600

    Move left script

--- Prototype 3.0/Assets/Scripts/MoveLeft.cs
+++ Prototype 3.0/Assets/Scripts/MoveLeft.cs
@@ -21,7 +21,6 @@ public class MoveLeft : MonoBehaviour
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
-
         if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
