6d29b7bd5600204910894bf254afdadcf50c3a65
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-03-28 22:54:12 -0600

    Added new Moving script and boundaries

--- New Project with Primitives/Assets/Scripts/PlayerController.cs
+++ New Project with Primitives/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
-
+    private float zBound = 6;
     private Rigidbody playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+    
+    // Moves the player based on the arrow key input
+    void MovePlayer()
+    { 
+        float horizontalInput = Input.GetAxis("Horizontal"); 
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
     }
+    // Prevent the player from leaving the rop or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        { 
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        
+        if (transform.position.z > zBound)
+        {
+            
+        }
+    }
+
 }
