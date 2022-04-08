7524e8299913d6af906a0c5079d334f1a4916c67
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-04-07 20:58:13 -0600

    Fixing scripts

--- Prototype(5)/Assets/Scripts/Target.cs
+++ Prototype(5)/Assets/Scripts/Target.cs
@@ -28,6 +28,16 @@ public class Target : MonoBehaviour
         
     }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEneter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
