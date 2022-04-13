4ddce8a52d116e827cf630d41b590b523e49a157
UVU_DGM1610_spring <myboyz117@gmail.com>
2022-04-13 13:14:37 -0600

    Fixed Script for Player and Enemy

--- New Project with Primitives/Assets/Scripts/PlayerController.cs
+++ New Project with Primitives/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
-    private Rigidbody playerRb;
     private float zBound = 10;
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    // Moves the palyer based on arrow key input
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
 
+    // Prevent the player from leaving hte top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
         if (transform.position.z < -zBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
@@ -32,5 +43,21 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy.");
+        }
+    }
+  
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Powerup"))
+       {
+           Destroy(other.gameObject);
+       }
     }
 }
